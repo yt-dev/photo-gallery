41db48fe5719225682e4099a274e7e053ae4769c
YT Huang <ytzhbit@gmail.com>
2020-08-10 09:40:53 +0800

    cleanup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,10 @@
 import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import Title from "./components/Title";
 
-function App() {
-  return (
-    <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>Hello React!</p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+  <div className='App'>
+    <Title />
+  </div>
+);
 
 export default App;
